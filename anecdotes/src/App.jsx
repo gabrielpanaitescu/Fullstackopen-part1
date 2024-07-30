730ccb81715bd907a797a24434a447193fe47bbf
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-07-31 02:34:24 +0300

    Complete 1.14 (arr method)

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -2,24 +2,16 @@ import { useState } from "react";
 import anecdotes from "./anecdotes.js";
 
 const App = () => {
-  const [anecdotesObj, setAnecdotesObj] = useState(
-    anecdotes.reduce((accum, currValue, index) => {
-      accum[index] = { anecdote: currValue, points: 0 };
-      return accum;
-    }, {})
-  );
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(8).fill(0));
+
+  const maxValue = Math.max(...points);
+  const topAnecdote = anecdotes[points.indexOf(maxValue)];
 
   const handleVote = () => {
-    setAnecdotesObj((prevAnecdotesObj) => {
-      return {
-        ...prevAnecdotesObj,
-        [selected]: {
-          anecdote: prevAnecdotesObj[selected].anecdote,
-          points: prevAnecdotesObj[selected].points + 1,
-        },
-      };
-    });
+    const copyPoints = [...points];
+    copyPoints[selected] = copyPoints[selected] + 1;
+    setPoints(copyPoints);
   };
 
   const handleNext = () => {
@@ -27,31 +19,23 @@ const App = () => {
     setSelected(randomNumber);
   };
 
-  const topAnecdoteObj = Object.values(anecdotesObj).reduce(
-    (accum, currValue) => {
-      if (currValue.points > accum.points) return currValue;
-      return accum;
-    },
-    anecdotesObj[0]
-  );
-
   return (
     <div>
       <section>
         <h2>Anecdote of the day</h2>
-        <div>{anecdotesObj[selected].anecdote}</div>
-        <p>has {anecdotesObj[selected].points} points</p>
+        <div>{anecdotes[selected]}</div>
+        <p>has {points[selected]} points</p>
         <button onClick={handleVote}>vote</button>
         <button onClick={handleNext}>next anecdote</button>
       </section>
       <section>
         <h2>Anecdote with most votes</h2>
-        {topAnecdoteObj.points < 1 ? (
+        {maxValue < 1 ? (
           <p>No votes registered yet</p>
         ) : (
           <>
-            <p>{topAnecdoteObj.anecdote}</p>
-            <p>has {topAnecdoteObj.points} points</p>
+            <p>{topAnecdote}</p>
+            <p>has {maxValue} points</p>
           </>
         )}
       </section>
