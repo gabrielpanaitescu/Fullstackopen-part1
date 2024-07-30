5645ac466d293226d4a91bc3e4168521872ca31e
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-07-30 21:34:29 +0300

    Complete exercises 1.7, 1.8, 1.9

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -4,13 +4,14 @@ const FeedbackButton = ({ text, handleClick }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const Stats = ({ text, value }) => {
+const Statistics = ({ text, value, children }) => {
   return (
-    <>
-      <p>
-        {text} {value}
-      </p>
-    </>
+    <p>
+      {Number.isNaN(value)
+        ? `Reviews are needed in order to calculate ${text}`
+        : `${text} ${value}`}{" "}
+      {children}
+    </p>
   );
 };
 
@@ -19,22 +20,39 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
 
+  const total = bad + good + neutral;
+  const average = (good - bad) / total;
+  const positive = good / total;
+
   return (
     <main>
       <section>
         <h1>give feedback</h1>
-        <FeedbackButton text={"good"} handleClick={() => setGood(good + 1)} />
-        <FeedbackButton text={"bad"} handleClick={() => setBad(bad + 1)} />
-        <FeedbackButton
-          text={"neutral"}
-          handleClick={() => setNeutral(neutral + 1)}
-        />
+        <div className="button-container">
+          <FeedbackButton text={"good"} handleClick={() => setGood(good + 1)} />
+          <FeedbackButton text={"bad"} handleClick={() => setBad(bad + 1)} />
+          <FeedbackButton
+            text={"neutral"}
+            handleClick={() => setNeutral(neutral + 1)}
+          />
+        </div>
       </section>
       <section>
         <h1>statistics</h1>
-        <Stats text={"good"} value={good} />
-        <Stats text={"bad"} value={bad} />
-        <Stats text={"neutral"} value={neutral} />
+        {total === 0 ? (
+          <p>No feedback given</p>
+        ) : (
+          <div className="statistics-container">
+            <Statistics text={"good"} value={good} />
+            <Statistics text={"bad"} value={bad} />
+            <Statistics text={"neutral"} value={neutral} />
+            <Statistics text={"all"} value={total} />
+            <Statistics text={"average"} value={average} />
+            <Statistics text={"positive percentage"} value={positive * 100}>
+              %
+            </Statistics>
+          </div>
+        )}
       </section>
     </main>
   );
